07a0b1097b85ac7ade4d1d32cd531e2b782ce038
liguanlin <517445163@qq.com>
2021-04-26 17:31:14 +0800

    add a solution

--- make_a_video_from_img_files.py
+++ make_a_video_from_img_files.py
@@ -10,6 +10,9 @@ print(len(img_path_name_list))
 print(type(img_path_name_list))
 print(img_path_name_list[0])
 video = cv2.VideoWriter('D:/DataSets/hyperspectraldatasets/424capture/lowlight_Apple_2021-04-24_08-42-16/hyper.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, (640, 480))
+"""
+方案1：
+
 for i in range(len(img_path_name_list)):
 
     img = io.imread(file_dir + img_path_name_list[i])
@@ -20,7 +23,22 @@ for i in range(len(img_path_name_list)):
     img = cv2.resize(img, (640, 480))
     print(img.shape)
     #img_color = np.stack((img, img, img), 2)
-    img_color = gray2rgb(img)
+    img_color = gray2rgb(img) #一定要转换成rgb格式才可以写入cv2.VideoWriter
+    video.write(img_color)
+"""
+
+for i in range(len(img_path_name_list)):
+
+    #img = io.imread(file_dir + img_path_name_list[i])
+    img = cv2.imread(file_dir + img_path_name_list[i], cv2.IMREAD_UNCHANGED)
+    img = img.astype(np.float32)
+    img = (img/2047)*255
+    img = img.astype(np.uint8)
+
+    img = cv2.resize(img, (640, 480))
+    print(img.shape)
+
+    img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB) ##一定要转换成rgb24格式才可以写入cv2.VideoWriter
     video.write(img_color)
 
 video.release()
